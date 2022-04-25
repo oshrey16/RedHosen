dcd34c6ccd0e6ddb5049911a98ffe4e31e1d0569
Oshrey Avraham <oshrey16@gmail.com>
2022-04-25 20:01:18 +0300

    remove import

--- lib/Admins/manageUsers/user_info.dart
+++ lib/Admins/manageUsers/user_info.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:flutter/widgets.dart';
 import 'package:red_hosen/mytools.dart';
 
 class UserInfo extends StatefulWidget {
