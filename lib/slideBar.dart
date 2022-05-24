3cfa2054cb05f7c4e487ec055998392d723ae18c
Oshrey Avraham <oshrey16@gmail.com>
2022-05-24 03:13:05 +0300

    Add MOKED phone

--- lib/slideBar.dart
+++ lib/slideBar.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:red_hosen/auth_services.dart';
 import 'package:red_hosen/contactus.dart';
 import 'package:red_hosen/main.dart';
+import 'package:red_hosen/mytools.dart';
 import 'package:red_hosen/video_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -16,7 +17,7 @@ class NavDrawer extends StatelessWidget {
         children: <Widget>[
           const DrawerHeader(
             child: Text(
-              'מסך זה בבניה',
+              'תפריט',
               style: TextStyle(color: Colors.white, fontSize: 25),
             ),
             decoration: BoxDecoration(
@@ -28,48 +29,62 @@ class NavDrawer extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.input),
-            title: Text('מסך ראשי'),
-            onTap: () => {},
-          ),
-          ListTile(
-            leading: Icon(Icons.verified_user),
-            title: Text('פרופיל'),
+            leading: const Icon(Icons.input),
+            title: const Text('מסך ראשי'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: Icon(Icons.call),
-            title: Text('התקשר לאגף הרווחה'),
+            leading: const Icon(Icons.verified_user,color: Colors.green),
+            title: const Text('פרופיל'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: const Icon(Icons.call,color: Colors.cyan,),
+            title: const Text('התקשר לאגף הרווחה'),
             onTap: () {
               launchUrlString("tel://086620297");
             },
           ),
           ListTile(
-            leading: Icon(Icons.call),
-            title: Text('התקשר למרכז חוסן'),
+            leading: const Icon(Icons.call,color: Colors.cyan,),
+            title: const Text('התקשר למרכז חוסן'),
             onTap: () {
               launchUrlString("tel://086611140");
             },
           ),
+                    ListTile(
+            leading: const Icon(Icons.call,color: Colors.cyan),
+            title: const Text('התקשר למוקד שדרות'),
+            onTap: () {
+              launchUrlString("tel://086610991");
+            },
+          ),
           ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('דיווח על תקלה'),
+            leading: const Icon(Icons.settings),
+            title: const Text('דיווח על תקלה'),
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => const ContactUsPage()));
             },
           ),
           ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('מתן עזרה ראשונה - הדרכה'),
+            leading: const Icon(Icons.border_color),
+            title: const Text('מתן עזרה ראשונה - הדרכה'),
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => const VideoPage()));
             },
           ),
+                    ListTile(
+            leading: const Icon(Icons.border_color),
+            title: const Text('הודעות אישיות'),
+            onTap: () {
+              showDialogMsg(context, MsgType.alert, "בקרוב!");
+            },
+          ),
           ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('התנתק מהמערכת'),
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('התנתק מהמערכת'),
             onTap: () {
               AuthService().logout();
               Navigator.of(context).pushReplacement(
