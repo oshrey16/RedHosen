73864cd9314d2fde2a34981573a574855a041990
Oshrey Avraham <oshrey16@gmail.com>
2022-06-12 11:57:53 +0300

    update mainPage Admins

--- lib/Admins/adminHosen/mainManage.dart
+++ lib/Admins/adminHosen/mainManage.dart
@@ -20,52 +20,121 @@ class _MainMagnageHosenPageState extends State<MainMagnageHosenPage> {
         appBar: AppBar(title: const Text("ניהול משתמשים"), centerTitle: true),
         body: Container(
             alignment: Alignment.center,
-            padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+            // padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
             child: SingleChildScrollView(
                 child: Column(children: [
-              SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const UserConfirmation(type: UserType.hosen)),
-                      );
-                    },
-                    child: const Text("אישור מטפלים במערכת"),
-                  )),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.green.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("אישור מטפלים",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_alt_rounded,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserConfirmation(type: UserType.hosen)),
+                  );
+                },
+              ),
               const SizedBox(height: 15),
-              SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const UserRejectionPage(type: UserType.hosen)),
-                      );
-                    },
-                    child: const Text("מחיקת מטפלים במערכת"),
-                  )),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.orange.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            
+                                const Text("מחיקת מטפלים",
+                                    style: TextStyle(fontSize: 18)),
+                          
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_disabled_outlined,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserRejectionPage(type: UserType.hosen)),
+                  );
+                },
+              ),
               const SizedBox(height: 15),
-              SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: Colors.red),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AddAdminPage(
-                                  type: UserType.hosen,
-                                )),
-                      );
-                    },
-                    child: const Text("הוספת מנהלים למערכת"),
-                  )),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.red,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                   
+                                const Text("הוספת מנהל מערכת",
+                                    style: TextStyle(fontSize: 18)),
+                              
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.manage_accounts,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AddAdminPage(
+                              type: UserType.hosen,
+                            )),
+                  );
+                },
+              ),
             ]))));
   }
 }
--- lib/Admins/adminSocial/mainManage.dart
+++ lib/Admins/adminSocial/mainManage.dart
@@ -23,83 +23,185 @@ class _MainMagnageSocialPageState extends State<MainMagnageSocialPage> {
             padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
             child: SingleChildScrollView(
                 child: Column(children: [
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const UserConfirmation(type: UserType.social)),
-                    );
-                  },
-                  child: const Text("אישור עובדים סוציאלים במערכת"),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.green.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("אישור עובדים סוציאלים",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_alt_rounded,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserConfirmation(type: UserType.social)),
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const UserRejectionPage(type: UserType.social)),
-                    );
-                  },
-                  child: const Text("מחיקת עובדים סוציאלים במערכת"),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.orange.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("מחיקת עובדים סוציאלים",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_disabled_outlined,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserRejectionPage(type: UserType.social)),
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const UserConfirmation(type: UserType.reporter)),
-                    );
-                  },
-                  child: const Text("אישור צח\"ש במערכת"),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.green.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("אישור צח\"ש",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_alt_rounded,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserConfirmation(type: UserType.reporter)),
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const UserRejectionPage(type: UserType.reporter)),
-                    );
-                  },
-                  child: const Text("מחיקת צח\"ש במערכת"),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.orange.shade400,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("מחיקת צח\"ש",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.person_add_disabled_outlined,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const UserRejectionPage(type: UserType.reporter)),
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AddAdminPage(
-                                  type: UserType.social,
-                                )),
-                      );
-                    },
-                    child: const Text("הוספת מנהלים למערכת"),
-                  )),
+              GestureDetector(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.red,
+                  elevation: 14.0,
+                  child: Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("הוספת מנהלים למערכת",
+                                style: TextStyle(fontSize: 18)),
+                            Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 10, 0),
+                                child: const Icon(
+                                  Icons.manage_accounts,
+                                  color: Colors.white,
+                                  size: 40,
+                                ))
+                          ])),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const AddAdminPage(type: UserType.social)),
+                  );
+                },
+              ),
             ]))));
   }
 }
